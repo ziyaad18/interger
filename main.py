1f929cd65226fdadce26b599f8f8ac0fdd8ea4c7
ziyaad18 <ziyaadpetersen347@gmail.com>
2020-09-15 16:23:02 +0200

    spiderman

---
+++ main.py
@@ -0,0 +1,8 @@
+radius = int(input("Please enter your number"))
+pi = 3.142
+Area = pi * radius * radius
+print(Area)
+raius= int(input("please enter number"))
+pi= 2.1
+Area= pi * radius * radius
+print(Area)
